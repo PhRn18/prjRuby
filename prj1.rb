7275d6adecaace33eafa10100264fa1b9d702ca2
PhRn18 <paulinhohenriqueneves@hotmail.com>
2022-04-25 13:16:40 -0300

    Testando o interpretador

--- prj1.rb
+++ prj1.rb
@@ -1,1 +1,14 @@
 #Inicio de um novo projeto no ruby
+#teste interpretador 
+class Main
+    attr_accessor :name
+    def initialize(string)
+        @name = string
+    end
+    def teste_mostrar()
+        puts @name
+    end
+end
+
+teste = Main.new("a")
+teste.teste_mostrar()
